f8504d16c6c2d19a48719527494991226ef6354f
JevinJ <jevinjones@openmailbox.org>
2018-07-24 15:09:32 -0700

    alphabetized imports, removed old/unneeded yandex variable, moved main_trigger to patterns.py, convert lang_list to list initially, made regex matching a bit nicer, some renaming.

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-import requests
+from itertools import chain
 import praw
-import time
 import re
+import requests
+import sqlite3
+import time
+from modules import patterns
+from modules.blacklist import *
 from modules.messages import syntax_message, signature, un_blacklist_message
 from modules.translate import *
-from modules.blacklist import *
-import sqlite3
+
 
 conn = sqlite3.connect('bot.db')
 c = conn.cursor()
-
-# https://translate.yandex.net/api/v1.5/tr.json/translate?key={parseconfig.yandex_api}&text={text}&format=plain&lang=ru
 
 # Authenticating with Reddit
 reddit = praw.Reddit('translate_bot')
@@ -22,11 +23,12 @@ comments = reddit.subreddit('all').stream.comments(pause_after=-1)
 inbox_stream = praw.models.util.stream_generator(reddit.inbox.unread, pause_after=-1)
 
 # Defines
-main_trigger = '@translate' # Bot replies to comment with this keyword.
 executed_timestamp = time.time() # Stores the current time in seconds since the Epoch. Used later to prevent the bot from replying to old comments everytime it is executed.
 
 blacklisted = c.execute('SELECT * from Blacklist').fetchall()
 blacklisted_subs = [row[0] for row in blacklisted]
+
+lang_list = list(chain(*c.execute('SELECT * from CountryCodes').fetchall()))
 
 
 while True:
@@ -37,22 +39,14 @@ while True:
 
             if comment.created_utc > executed_timestamp:
                 if comment.subreddit not in blacklisted_subs:
-                    if str(comment.body).startswith(main_trigger):
+                    if str(comment.body).startswith(patterns.main_trigger):
                         if comment.parent_id == comment.link_id:                    
                             author = str(comment.author)
 
-                            # Regular expressions for the parameters.
-                            # Comment must follow this structure: main_trigger language(max 2 chars) query(~)
-                            lang_re = '[a-z]{2}'
-                            query_re = '.+'
-
-                            pattern = re.search(r'{0} ({1}) ({2})'.format(main_trigger, lang_re.lower(), query_re), comment.body)
+                            comment_match = re.search(patterns.full_trigger_pattern, comment.body)
                             
                             try:
-                                lang = pattern.group(1) # pattern({1})
-                                query = pattern.group(2) # pattern({2})
-
-                                lang_list = c.execute('SELECT * from CountryCodes').fetchall()
+                                lang, query = comment_match.groups()
                                 codes = [row[0] for row in lang_list]
 
                                 if str(lang) not in codes: # If the parameters passed to "lang" is not recognized by Yandex, send a message to the user notifying him.
