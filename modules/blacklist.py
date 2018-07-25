ab8e1f3f7c66ed1175c6e7f1ba300970ffbb28b8
JevinJ <jevinjones@openmailbox.org>
2018-07-24 19:21:57 -0700

    add blacklist add/remove functions. get_mod_list() name changed and returns set, will now keep trying for connection if failed.

--- modules/blacklist.py
+++ modules/blacklist.py
@@ -1,19 +1,40 @@
 import praw
+import requests
 from requests_html import HTMLSession
+from time import sleep
 
 url = 'https://old.reddit.com/r/{}/about/moderators'
 session = HTMLSession()
 
-def get_mod_list(sub):
-    
-    mods = []
 
-    r = session.get(url.format(sub))
+def remove_from_blacklist(**kwargs):
+    sub_name = kwargs['sub_name']
+    kwargs['sql_cursor'].execute('DELETE FROM Blacklist WHERE SubName = ?', (sub_name,))
+    kwargs['sql_connection'].commit()
 
-    if r.status_code == 200:
-        users = r.html.find('span.user')
-        for user in users[1:]:
-            moderator = user.text.split("\xa0")[0]
-            mods.append(moderator)
 
-        return mods
+def add_to_blacklist(**kwargs):
+    sub_name = kwargs['sub_name']
+    kwargs['sql_cursor'].execute('INSERT INTO Blacklist(SubName) VALUES (?)', (sub_name,))
+    kwargs['sql_connection'].commit()
+
+
+def get_moderator_set(sub):
+    mods = set()
+
+    response = None
+    while response is None:
+        try:
+            response = session.get(url.format(sub))
+            response.raise_for_status()
+            users = response.html.find('span.user')
+            for user in users[1:]:
+                mod = user.text.split('\xa0')[0]
+                mods.add(mod)
+        except (requests.exceptions.HTTPError, ConnectionError) as exc:
+            sleep(5)
+    return mods
+
+
+
+
