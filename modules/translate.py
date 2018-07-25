a09fb0da377fea1a001c3cb990a5b0fcc782a182
JevinJ <jevinjones@openmailbox.org>
2018-07-24 23:21:37 -0700

    stylistic change - tightening code

--- modules/translate.py
+++ modules/translate.py
@@ -4,7 +4,6 @@ from time import sleep
 
 def translate(lang, text):
     url = f'https://translate.yandex.net/api/v1.5/tr.json/translate?key={yandex_api}&lang={lang}&text={text}&format=plain'
-
     response = None
     while response is None:
         try:
